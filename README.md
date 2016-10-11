e89c82d0a418f4493bea19050d51ba07c4aea684
Jason johnson <jason_johnson@sweetwater.com>
2016-10-10 21:10:47 -0400

    init commit v1

---
+++ README.md
@@ -0,0 +1,12 @@
+IMAP Testing Tool
+
+Will download the first 20 messages in an inbox while timing the results
+
+Usage: imap-test.py [OPTIONS]
+
+Options:
+  --host TEXT      IMAP Host FQDN.
+  --username TEXT  IMAP Username.
+  --password TEXT  IMAP Password.
+  --folder TEXT    IMAP Folder ie Inbox.
+  --help           Show this message and exit.
---
+++ imap-test.py
@@ -0,0 +1,63 @@
+import sys
+import imaplib
+from timeit import default_timer as timer
+import time
+import click
+
+
+def process_mailbox(M):
+    rv, data = M.search(None, "ALL")
+    if rv != 'OK':
+        print "No messages found!"
+        return
+    fetch_loop_start = timer()
+    for num in data[0].split():
+        if int(num) < 21:
+            rv, data = M.fetch(num, '(RFC822)')
+            if rv != 'OK':
+                print "ERROR getting message", num
+                return
+    fetch_loop_end = timer()
+    fetch_loop_time = fetch_loop_end - fetch_loop_start
+    print "Loop Time: " + "%.9f" % fetch_loop_time
+
+def print_usage_and_die():
+    print "Usage: imap-test.py --help"
+    exit(1)
+
+@click.command()
+@click.option('--host', default="imap.gmail.com", help='IMAP Host FQDN.')
+@click.option('--username', default="", help='IMAP Username.')
+@click.option('--password', default="", help='IMAP Password.')
+@click.option('--folder', default="Inbox", help='IMAP Folder ie Inbox.')
+
+
+def main(host, username, password, folder):
+
+    #Check Args
+    if username == "":
+        print_usage_and_die()
+    if password == "":
+        print_usage_and_die()
+
+    M = imaplib.IMAP4_SSL(host)
+    try:
+        login_start = timer()
+        rv, data = M.login(username, password)
+        time.sleep(5)
+        login_end = timer()
+        login_time = login_end - login_start
+        print "Login Time: " + "%.9f" % login_time
+    except imaplib.IMAP4.error:
+        print "LOGIN FAILED!!! "
+        sys.exit(1)
+    rv, data = M.select(folder)
+    if rv == 'OK':
+        process_mailbox(M)
+        M.close()
+    else:
+        print "ERROR: Unable to open mailbox ", rv
+    M.logout()
+
+if __name__ == '__main__':
+    main()
